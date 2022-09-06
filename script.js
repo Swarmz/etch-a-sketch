ea0c3abaa0a570ece28f7d694bf9696b22283df1
gavin <gavinsutton6511@gmail.com>
2022-09-06 13:16:49 +0900

    Introduce working 256x256 populating method

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function createDiv() {
+    const container = document.querySelector('.container');
+    for (i = 0; i < 256; i++) {
+        const content = document.createElement('div');
+        content.classList.add('content');
+        container.appendChild(content);
+    }
+}
+
+createDiv();
