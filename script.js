dd88cf8ef361f3ef6cb04530358cec2cfc9c490e
gavin <gavinsutton6511@gmail.com>
2022-09-06 13:31:12 +0900

    Add mouseover function to allow drawing

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function createDiv() {
     for (i = 0; i < 256; i++) {
         const content = document.createElement('div');
         content.classList.add('content');
+        content.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'black';
+          });
         container.appendChild(content);
     }
 }
